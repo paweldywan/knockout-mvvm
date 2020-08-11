a43c19dd706720a4389d260227174583da0dfbba
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-08-11 22:26:31 +0200

    Fix

--- materials/knockout-mvvm-m5-exercise-files/Code/before/KnockoutMVVM/KnockoutMVVM/Scripts/5-07-afterAdd-beforeRemove-and-arrayFilter.js
+++ materials/knockout-mvvm-m5-exercise-files/Code/before/KnockoutMVVM/KnockoutMVVM/Scripts/5-07-afterAdd-beforeRemove-and-arrayFilter.js
@@ -31,10 +31,13 @@
         self.isSelected = ko.computed(function () {
             return selectedItem() === self;
         });
+        self.isGuitar = ko.computed(function () {
+            return this.category() ? this.category().id() === 1 : false;
+        }, self);
         //this.shortDescription = ko.observable();
         self.shortDesc = ko.computed(function () {
             return this.model() ? this.model().brand() + " " + this.model().name() : "";
-        }, self),
+        }, self);
         self.photoUrl = ko.computed(function () {
             return photoPath + this.photo();
         }, self);
@@ -50,45 +53,71 @@
             }
         },
             products = ko.observableArray([]),
+            showAll = ko.observable(),
+            productsToShow = ko.computed(function () {
+                if (showAll()) {
+                    return products();
+                }
+
+                return ko.utils.arrayFilter(products(), function (p) {
+                    return p.isGuitar();
+                });
+            }),
             selectedProduct = ko.observable(),
+            showProducts = function (elem) {
+                if (elem.nodeType === 1) {
+                    $(elem).hide().slideDown();
+                }
+            },
+            hideProducts = function (elem) {
+                if (elem.nodeType === 1) {
+                    $(elem).slideUp(function () {
+                        $(elem).remove();
+                    });
+                }
+            },
             showDetails = ko.observable(false),
             //private custom sort function
-            sortFunction = function(a, b) {
+            sortFunction = function (a, b) {
                 return a.shortDesc().toLowerCase() > b.shortDesc().toLowerCase() ? 1 : -1;
             },
-            selectProduct = function(p) {
+            selectProduct = function (p) {
                 selectedProduct(p);
-            };
-        loadProducts = function () {
-            $.each(my.sampleData.data.Products, function (i, p) {
-                products.push(new my.Product(selectedProduct)
-                            .id(p.Id)
-                            .salePrice(p.SalePrice)
-                            .photo(p.Photo)
-                            .category(new my.Category()
-                                .id(p.Category.Id)
-                                .name(p.Category.Name)
-                            )
-                            .model(new my.Model()
-                                .id(p.Model.Id)
-                                .name(p.Model.Name)
-                                .brand(p.Model.Brand)
-                            )
-                            .description(p.Description)
-                            .rating(p.Rating)
+            },
+            loadProducts = function () {
+                $.each(my.sampleData.data.Products, function (i, p) {
+                    products.push(new my.Product(selectedProduct)
+                        .id(p.Id)
+                        .salePrice(p.SalePrice)
+                        .photo(p.Photo)
+                        .category(new my.Category()
+                            .id(p.Category.Id)
+                            .name(p.Category.Name)
+                        )
+                        .model(new my.Model()
+                            .id(p.Model.Id)
+                            .name(p.Model.Name)
+                            .brand(p.Model.Brand)
+                        )
+                        .description(p.Description)
+                        .rating(p.Rating)
                     );
-            });
-            products().sort(sortFunction);
-        };
+                });
+                products().sort(sortFunction);
+            };
         return {
             metadata: metadata,
             products: products,
             selectedProduct: selectedProduct,
             showDetails: showDetails,
             selectProduct: selectProduct,
-            loadProducts: loadProducts
+            loadProducts: loadProducts,
+            showAll: showAll,
+            productsToShow: productsToShow,
+            showProducts: showProducts,
+            hideProducts: hideProducts
         };
-    } ();
+    }();
 
     my.vm.loadProducts();
     ko.applyBindings(my.vm);
